e8faece6eb3f75a674c5fc369bf0523c0aa48a7d
raleel <raleel@gmail.com>
2025-12-11 19:32:53 -0800

    many options added

--- any-options-cfi.js
+++ any-options-cfi.js
@@ -1,10 +1,8 @@
 const ANY_OPTIONS = {
-  Art: ["Painting", "Sculpture", "Poetry", "Storytelling", "Calligraphy"],
+  Art: ["Painting", "Sculpture", "Poetry", "Storytelling", "Calligraphy", "Literature"],
   Craft: ["Blacksmith", "Carpentry", "Weaving", "Pottery", "Brewery"],
-  Culture: ["Barbarian", "Civilised", "Nomadic", "Primitive", "Imperial", "Border Kingdoms"],
-  Language: ["Korantine", "Lyserian", "Thennalt", "Common Trade", "Tarak"],
-  Lore: ["History", "Theology", "Regional", "Tactics", "Arcana"],
-  Science: ["Biology", "Chemistry", "Physics", "Xenology"],
+  Culture: ["Elf", "Dwarf", "Halfling", "Gnome", "Barbarian", "Civilized", "Primitive", "Nomadic"],
+  Language: ["Ancient Common", "Ancient Pharaonic", "Brownie", "Bruxxic", "Carnivorous ape", "Celestial", "Centaur", "Drau", "Demon", "Devil", "Infernal", "Draconic", "Druids’ Cant", "Dwarvish", "Dryad", "Air Elemental", "Earth Elemental", "Water Elemental", "Fire Elemental", "Elvish", "Ettin", "Faun", "Flind", "Gargoyle", "Genie", "Gnoll", "Gnome", "Goblin", "Gorgon", "Halfling", "Giant", "Hobgoblin", "Kobold", "Lizardman", "Manticore", "Nixie", "Nymph", "Orcish", "Ogre", "Pixie", "Satyr", "Small burrowing animals", "Sprite", "Thieves’ cant", "Tree'nt", "Troglodyte", "Troll", "Underdeep", "Werebear", "Wereboar", "Wererat", "Weretiger", "Werewolf", "Mimic", "Beyonder", "Invisible Stalker", "Unicorn"],
+  Lore: ["Alchemy", "History", "Theology", "Regional", "Astrology", "Astronomy", "Geography", "History", "Monsters", "Mythology", "Politics", "Strategy and Tactics", "Arcana"],
   Survival: ["Forest", "Desert", "Tundra", "Jungle", "Urban"],
-  Customs: ["Imperial Law", "Tribal Rites", "Temple Etiquette"],
 };
